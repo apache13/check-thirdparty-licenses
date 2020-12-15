fb94fdc0b8b3832394dcb94fe23f8fde6a1263df
nuttapon.pini <nuttapon.pini@wealth.co.th>
2020-12-15 20:45:37 +0700

    initial spring boot

---
+++ src/main/java/com/apache13/example/checklicense/CheckLicenseApplication.java
@@ -0,0 +1,13 @@
+package com.apache13.example.checklicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckLicenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckLicenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apache13/example/checklicense/CheckLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache13.example.checklicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckLicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
